c17bfb60736a9a7798d0dabcb172f2ce8b189245
Melchior Reihlen-Boergers <m.reihlenboergers@gmail.com>
2023-08-26 10:08:17 +0200

    jsut a test

---
+++ testfile3.py
@@ -0,0 +1,16 @@
+class my_class(object):
+    """
+    Supe duper class
+    """
+    def __init__(self, x, y) -> None:
+        
+        self.x = x 
+        self.y = y
+
+    def add(self):
+
+        """
+        Function that adds two values
+        """
+        return self.x + self.y
+    
